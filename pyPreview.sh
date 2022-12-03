a53d63f003ecebf7c3465c4c1a5f850b885e65f9
Connor Petri <114629025+connor-petri@users.noreply.github.com>
2022-12-02 16:52:29 -0800

    added bash script and updated setup.py

---
+++ pyPreview.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cd ~/pyPreview
+
+python main.py
+
+while [ 1 ] ; do 
+	sleep 1
+done
--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from os import system
 
+
 if __name__ == "__main__":
-  system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
-  system("pip3 install pysimplegui")
-  system("brew install tcl-tk")
-  system("clear")
-  print("\nDone!\n")
+    system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+
+    system("brew install pyenv")
+    system("brew install tcl-tk")
+
+    # pyenv setup
+    system("pyenv install 3.10.6")
+    system("pyenv virtualenv 3.10.6 pyPreview")
+    system("pyenv activate pyPreview")
+
+    # install; packages
+    system("pip install pysimplegui")
+
+    # make bash script on desktop to run main.py
+    system("cp pyPreview.sh ~/Desktop")
+    system("chmod +x ~/Desktop/pyPreview.sh")
+
+    # system("clear")
+    print("\nDone!\n")
